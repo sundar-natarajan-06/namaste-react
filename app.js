ab81909b305a32ba9ec5b33e126043bf80480b5d
Sundar Natarajan <sundar@talentmicro.com>
2023-12-29 14:13:00 +0530

    added console log

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ const child2 = React.createElement("div", { id: "child2" }, [heading1, heading2]
 const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+console.log(parent);
